3e79619582dda4d687e61c9a01c1c9ba87815a72
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2024-04-03 23:16:59 +0200

    ➕ added Initial classes code (#6)
    * ➕ added Initial classes code
    
    * ➕ Added more parameters

---
+++ UnitClasses/AlienMonster.h
@@ -0,0 +1,9 @@
+#ifndef ALIENMONSTER_H
+#define ALIENMONSTER_H
+
+#include "Unit.h"
+class AlienMonster: public Unit
+{
+
+};
+#endif
---
+++ UnitClasses/Unit.h
@@ -0,0 +1,36 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+enum UnitType
+{
+	ES, EG, ET,
+	AS, AD, AM
+};
+class Unit
+{
+	UnitType unitType;
+	int id;
+
+	int Tj; // Join time 
+	int Ta; // First attack time
+	int Td; // Destruction time
+
+	int Df; // First attack delay
+	int Dd; // Destruction delay
+	int Db; // Battle time
+
+	int health;
+	int power;
+	int attackCapacity;
+public:
+	void recieveDamage(int loss);
+	virtual void print() = 0;
+	virtual void attack(Unit* aUnit) = 0;  //shouldn't it be passed a list??? will check later
+
+};
+#endif
+
+void Unit:: recieveDamage(int loss)
+{
+	health -= loss;
+}
