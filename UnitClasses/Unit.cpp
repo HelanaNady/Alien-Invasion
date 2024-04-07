93b436f7e417dac0ce4b92ecd7aaf07eb489a5f7
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-04-07 22:48:24 +0200

    :memo: Removed unnecessary commented lines

--- UnitClasses/Unit.cpp
+++ UnitClasses/Unit.cpp
@@ -16,7 +16,7 @@ Unit::Unit(Game* gamePtr, UnitType unitType, int health, int power, int attackCa
 		id = ++lastAlienId;
 		armyType = ArmyType::ALIEN;
 	}
-	
+
 	Tj = gamePtr->getCurrentTimestep();
 }
 
@@ -75,9 +75,6 @@ void Unit::setAttackCapacity(int attackCapacity)
 
 std::ostream& operator<<(std::ostream& oStream, Unit* unitObj)
 {
-	// oStream << unitObj->id << "\t" << unitObj->Tj << std::endl;
-	// oStream << unitObj->health << "\t" << unitObj->power << "\t" << unitObj->attackCapacity << std::endl;
-	
 	oStream << unitObj->id;
 
 	return oStream;
