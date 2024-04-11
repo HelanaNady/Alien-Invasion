24b0bfb494837d1cbfbd4db12ad0d26ba3d87e83
HelanaNady <helananady7@gmail.com>
2024-04-11 03:31:22 +0200

    :heavy_plus_sign: Added attack function of drones

--- UnitClasses/AlienDrone.cpp
+++ UnitClasses/AlienDrone.cpp
@@ -1,4 +1,5 @@
 #include "AlienDrone.h"
+#include "../Game.h"
 
 AlienDrone::AlienDrone(Game* gamePtr, int health, int power, int attackCapacity)
     : Unit(gamePtr, UnitType::AD, health, power, attackCapacity)
@@ -12,4 +13,75 @@ void AlienDrone::print() const
 }
 
 void AlienDrone::attack()
-{}
+{
+    int ETnumber = (attackCapacity / 2) + 1;
+    int EGnumber = attackCapacity - ETnumber;
+
+    LinkedQueue<Unit*> ETlist = gamePtr->getEnemyList(EARTH, ET, ETnumber);
+    LinkedQueue<Unit*> EGlist = gamePtr->getEnemyList(EARTH, EG, EGnumber);
+
+    ArrayStack<Unit*> ETtempList;
+    LinkedQueue<Unit*> EGtempList;
+
+    for (int i = 0; i < attackCapacity; i++)
+    {
+        Unit* attackedUnit = nullptr;
+
+        if (i % 2 == 0)
+            ETlist.dequeue(attackedUnit);
+        else
+            EGlist.dequeue(attackedUnit);
+
+        if (!attackedUnit)
+            continue;
+
+        // Check if it was attacked before or not
+        if (attackedUnit->getTattack() == -1)
+            setTattack(gamePtr->getCurrentTimestep());
+
+        // Decrement health 
+        int newHealth = attackedUnit->getHealth() - calcUAP(attackedUnit);
+        attackedUnit->setHealth(newHealth);
+
+        // Check if it was killed or not
+        if (attackedUnit->getHealth())
+        {
+            UnitType unitType = attackedUnit->getUnitType();
+
+            switch (unitType)
+            {
+                case UnitType::ET:
+                    ETtempList.push(attackedUnit);
+                    break;
+
+                case UnitType::EG:
+                    EGtempList.enqueue(attackedUnit);
+                    break;
+            }
+        }
+        else
+        {
+            gamePtr->killUnit(attackedUnit);
+            attackedUnit->setTdestruction(gamePtr->getCurrentTimestep());
+        }
+
+        // Add to fought Units List
+        foughtUnits.enqueue(attackedUnit->getId());
+    }
+
+    // Re-adding attacked units to their original lists
+
+    while (!ETtempList.isEmpty())
+    {
+        Unit* unit;
+        ETtempList.pop(unit);
+        gamePtr->addUnit(unit);
+    }
+
+    while (!EGtempList.isEmpty())
+    {
+        Unit* unit;
+        EGtempList.dequeue(unit);
+        gamePtr->addUnit(unit);
+    }
+}
