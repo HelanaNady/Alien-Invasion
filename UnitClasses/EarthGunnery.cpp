b9a74161df78cf8a54b06c4e2d13cc7dbfc6f2cc
Helana Nady <helananady7@gmail.com>
2024-04-13 11:16:02 +0200

    ✨ Implemented Earth Gunnery Attack Logic (#24)
    * :heavy_plus_sign: Added EG attack function
    
    * :hammer: Added missing enums
    
    * :zap: Updated setters and getters names
    
    * :recycle: Refactored temp list code
    
    * :bug: Fixed a typo
    
    * :bug: Fixed counter condition
    
    ---------
    
    Co-authored-by: Ahmed Sobhy <samahmedsobhy@gmail.com>

--- UnitClasses/EarthGunnery.cpp
+++ UnitClasses/EarthGunnery.cpp
@@ -1,4 +1,5 @@
 #include "EarthGunnery.h"
+#include "../Game.h"
 
 EarthGunnery::EarthGunnery(Game* gamePtr, int health, int power, int attackCapacity)
     : Unit(gamePtr, UnitType::EG, health, power, attackCapacity)
@@ -12,7 +13,67 @@ void EarthGunnery::print() const
 }
 
 void EarthGunnery::attack()
-{}
+{
+    int AMnumber = (attackCapacity / 2) + 1;
+    int ADnumber = attackCapacity - AMnumber;
+
+
+    LinkedQueue<Unit*> AMlist = gamePtr->getEnemyList(ArmyType::ALIEN, UnitType::AM, AMnumber);
+    LinkedQueue<Unit*> ADlist = gamePtr->getEnemyList(ArmyType::ALIEN, UnitType::AD, ADnumber);
+
+    LinkedQueue<Unit*> tempList;
+
+    int dronesCount = 2; // Counter to handle attacking 2 drones then a monster repeatedly
+
+    for (int i = 0; i < attackCapacity; i++)
+    {
+        Unit* attackedUnit = nullptr;
+
+        if (dronesCount == 0)
+        {
+            AMlist.dequeue(attackedUnit);
+            dronesCount = 2; // Reseting the counter
+        }
+        else
+        {
+            ADlist.dequeue(attackedUnit);
+            dronesCount--;
+        }
+
+        if (!attackedUnit && (!AMlist.isEmpty() || !ADlist.isEmpty()))
+        {
+            i--;
+            continue;
+        }
+
+        // Check if it was attacked before or not
+        if (attackedUnit->getFirstAttackTime() == -1)
+            attackedUnit->setFirstTimeAttack(gamePtr->getCurrentTimestep());
+
+        // Decrement health 
+        attackedUnit->recieveDamage(calcUAP(attackedUnit));
+
+        // Check if it was killed or not
+        if (attackedUnit->getHealth())
+        {
+            tempList.enqueue(attackedUnit);
+        }
+        else
+        {
+            gamePtr->killUnit(attackedUnit);
+            attackedUnit->setDestructionTime(gamePtr->getCurrentTimestep());
+            attackedUnit->setDestructionDelay();
+            attackedUnit->setBattleDelay();
+        }
+
+        foughtUnits.enqueue(attackedUnit->getId());
+    }
+
+    // Re-adding attacked units to their original lists
+    Unit* unit = nullptr;
+    while (tempList.dequeue(unit))
+        gamePtr->addUnit(unit);
+}
 
 int EarthGunnery::getPriority() const
 {
