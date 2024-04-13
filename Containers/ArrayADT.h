c4d49971604a8c991515c0a9e8ea15cc04607fc2
Helana Nady <helananady7@gmail.com>
2024-04-13 14:44:51 +0200

    :zap: Added ArrayADT class (#28)

---
+++ Containers/ArrayADT.h
@@ -0,0 +1,19 @@
+#ifndef ARRAY_ADT_H
+#define ARRAY_ADT_H
+
+template<typename T>
+class ArrayADT
+{
+public:
+
+    virtual bool isEmpty() const = 0;
+
+    virtual bool insert(const T& newEntry) = 0;
+
+    virtual bool remove(int index, T& entry) = 0;
+
+    virtual bool entryAt(T& entry, int index) const = 0;
+
+    virtual ~ArrayADT() {}
+};
+#endif 
