be735c69091bb5da30e7f7a27c74557be252d5f5
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-05-04 17:25:52 +0300

    :sparkles: Updated input file prompt

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
 
     // Ask the user to choose the game mode
     GameMode gameMode = INTERACTIVE;
-    int resultOption = -1;
+    int userChoice = -1;
 
     std::cout << std::endl;
     std::cout << "How do you want to witness humanity's last stand?" << std::endl;
@@ -22,12 +22,12 @@ int main()
     std::cout << "[2] Interactive Mode --> Do you want the aliens to know you're watching? Press enter for a timestep by step detailed results" << std::endl;
     std::cout << "Enter your choice (1/2): ";
 
-    while (resultOption != 1 && resultOption != 2)
+    while (userChoice != 1 && userChoice != 2)
     {
-        std::cin >> resultOption;
-        if (resultOption == 1)
+        std::cin >> userChoice;
+        if (userChoice == 1)
             gameMode = GameMode::SILENT;
-        else if (resultOption == 2)
+        else if (userChoice == 2)
             gameMode = GameMode::INTERACTIVE;
         else
         {
@@ -37,19 +37,37 @@ int main()
         }
     }
 
-    // Clear the input buffer
-    std::cin.ignore();
-
     // Ask the user to choose the input file
+    const int numInputFiles = 3;
+    std::string inputFiles[] = { "neutral", "strong_earth", "strong_alien" };
     std::string inputFileName;
+    userChoice = -1;
+
     std::cout << std::endl;
-    std::cout << "Enter the name of the input file" << std::endl;
-    std::cout << "Current options: neutral - strong_alien - strong_earth" << std::endl;
-    std::cin >> inputFileName;
+    std::cout << "Choose the input file:" << std::endl;
+    std::cout << "[1] Neutral --> Equal strength for both Earth and Aliens" << std::endl;
+    std::cout << "[2] Strong Earth --> Earth has a stronger army" << std::endl;
+    std::cout << "[3] Strong Aliens --> Aliens have a stronger army" << std::endl;
+    std::cout << "Enter your choice (1/2/3): ";
+
+    while (!(userChoice > 0 && userChoice <= numInputFiles))
+    {
+        std::cin >> userChoice;
+        if (userChoice > 0 && userChoice <= numInputFiles)
+            inputFileName = inputFiles[userChoice - 1];
+        else
+        {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Invalid input. Please enter 1, 2 or 3: ";
+        }
+    }
 
     // Ask the user to choose the output file
     std::string outputFileName;
-    std::cout << "Enter the name of the output file (e.g. BattleResults)" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Enter the name of the output file (e.g. BattleResults): ";
     std::cin >> outputFileName;
 
     // Clear the input buffer
@@ -57,7 +75,7 @@ int main()
 
     // Start the game
     Game game;
-    game.run(gameMode, "InputFiles/" + inputFileName+ ".txt", outputFileName + ".txt");
+    game.run(gameMode, "InputFiles/" + inputFileName + ".txt", outputFileName + ".txt");
 
     return 0;
 }
