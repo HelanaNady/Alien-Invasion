c5a1aea50b64565582455954cbd923e03a119a99
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-05-15 23:08:16 +0300

    :wrench: Transfer UML units to their armies at end game

--- Game.cpp
+++ Game.cpp
@@ -60,6 +60,9 @@ void Game::run(GameMode gameMode, const std::string& inputFileName, const std::s
 		}
 	} while (!battleOver(didArmiesAttack));
 
+	// Empty the unit maintenance list by returning the units to the appropriate army
+	emptyUnitMaintenanceList();
+
 	// Produce the output file
 	generateOutputFile(outputFileName);
 
@@ -128,6 +131,19 @@ void Game::printFinalResults() const
 	std::cout << "What a battle!" << std::endl;
 	std::cout << "Battle Result: " << battleResult() << std::endl;
 	std::cout << "Check the output file for a detailed conclusion" << std::endl;
+}
+
+void Game::emptyUnitMaintenanceList()
+{
+	HealableUnit* unit = nullptr;
+	int dummyPri = 0;
+
+	while (unitMaintenanceList.dequeue(unit, dummyPri))
+	{
+		addUnit(unit); // Add unit back to its army
+
+		unit = nullptr; // Nullify pointer
+	}
 }
 
 std::string Game::battleResult() const
@@ -329,9 +345,6 @@ GameStatistics Game::countStatistics()
 	// Killed Units Statistics
 	countKilledUnitsStatistics(gameStatistics);
 
-	// Unit Maintenance List
-	countUnitMaintenanceStatistics(gameStatistics);
-
 	return gameStatistics;
 }
 
@@ -417,44 +430,6 @@ void Game::countKilledUnitsStatistics(GameStatistics& gameStatistics)
 
 		// Nullify the pointer
 		unit = nullptr;
-	}
-}
-
-void Game::countUnitMaintenanceStatistics(GameStatistics& gameStatistics)
-{
-	HealableUnit* healableUnit = nullptr;
-	int count = unitMaintenanceList.getCount();
-	int priority = 0;
-
-	for (int i = 0; i < count; i++)
-	{
-		// Remove the unit from the maintenance list
-		unitMaintenanceList.dequeue(healableUnit, priority);
-
-		// Get the unit type & army type
-		ArmyType armyType = healableUnit->getArmyType();
-		UnitType unitType = healableUnit->getUnitType();
-
-		// Unit Counts
-		gameStatistics.unitCounts[unitType]++;
-		gameStatistics.totalUnitsCount++;
-
-		// Total Army Units Count
-		gameStatistics.armyStatistics[armyType].totalUnitsCount++;
-
-		// Count the infected Earth Soldiers
-		if (unitType == UnitType::ES)
-		{
-			EarthSoldier* earthSoldier = dynamic_cast<EarthSoldier*>(healableUnit);
-			if (earthSoldier->isInfected() || earthSoldier->isImmune()) // Check if the unit is infected or immune (has been infected before)
-				gameStatistics.totalInfectedESCount++;
-		}
-
-		// Add the unit back to the maintenance list
-		unitMaintenanceList.enqueue(healableUnit, priority);
-
-		// Nullify the pointer
-		healableUnit = nullptr;
 	}
 }
 
