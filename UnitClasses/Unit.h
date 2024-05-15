b6dbc532f78a42e7d1c86e752b94ada41fdaaa29
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-05-15 17:37:51 +0300

    :zap: Updated Unit class header file

--- UnitClasses/Unit.h
+++ UnitClasses/Unit.h
@@ -11,7 +11,7 @@ class Game;
 class Unit
 {
 private:
-	enum { MAX_EARTH_ID = 999, MAX_ALIEN_ID = 2999, MAX_EARTH_ALLIED_ID = 4999};
+	enum { MAX_EARTH_ID = 999, MAX_ALIEN_ID = 2999, MAX_EARTH_ALLIED_ID = 4999 }; // Maximum ID values for each army type
 
 	static int nextEarthId;
 	static int nextAlienId;
@@ -21,10 +21,10 @@ protected:
 	Game* gamePtr;
 	int id;
 
-	ArmyType armyType; 
-	UnitType unitType; 
+	ArmyType armyType;
+	UnitType unitType;
 
-	int Tj; // Join time 
+	int Tj; // Join time
 	int Ta; // First attack time
 	int Td; // Destruction time
 
@@ -33,10 +33,12 @@ protected:
 	int power; // Attack power
 	int attackCapacity; // Attack capacity
 
-	LinkedQueue<int> foughtUnits; // A list of the units fought in the current timestep to be printed 
+	LinkedQueue<int> foughtUnits; // A list of the units fought in the current timestep to be printed
 
 private:
-	void setHealth(double); // Health setter with range check & initial health saving
+	void setHealth(double); // Health setter with range check & initial health saving to be used in constructor
+	double calcUAP(Unit*) const; // Calculates the damage caused when attacked by "attackerUnit"
+	void clearFoughtUnits(); // Clear the list of fought units
 
 public:
 	Unit(Game*, UnitType, double, int, int);
@@ -44,49 +46,37 @@ public:
 	// Static functions
 	static bool cantCreateEarthUnit(); // Check if the max number of earth units is reached
 	static bool cantCreateAlienUnit(); // Check if the max number of alien units is reached
-	static bool cantCreateEarthAlliedUnit(); // Check if the max number of allied units is reached 
+	static bool cantCreateEarthAlliedUnit(); // Check if the max number of allied units is reached
 
 	void receiveDamage(double); // Decrease the health of the unit by "UAP"
 
-	double calcUAP(Unit*) const; // Calculates the damage caused when attacked by "attackerUnit"
-	virtual bool attack() = 0;
-	virtual void printFought() = 0;
+	virtual bool attack() = 0; // Attack the enemy units
+	virtual void printFought() = 0; // Print the units that the soldier has fought in the current timestep utilizing the foughtUnits list
 
 	virtual bool needsHeal() const; // Check if the unit is eligible for healing
 	bool isDead() const; // Check if the unit is dead
 	bool hasBeenAttackedBefore() const; // Check if it has been attacked before
 
-	void clearFoughtUnits(); // Clear the list of fought units
-
 	// Getters
 	int getId() const;
 	ArmyType getArmyType() const;
 	UnitType getUnitType() const;
-	double getInitialHealth() const;
 	double getHealth() const;
 	int getPower() const;
-	int getAttackCapacity() const;
 
-	// Time
+	// Time Getters
 	int getJoinTime() const;
-	int getFirstAttackTime() const;
 	int getDestructionTime() const;
 
-	// Delay
+	// Delay Getters
 	int getFirstAttackDelay() const;
 	int getDestructionDelay() const;
 	int getBattleDelay() const;
 
 	// Setters
-	void setPower(int);
-	void setAttackCapacity(int);
-
-	// Time
-	void setFirstTimeAttack(int);
 	void setDestructionTime(int);
 
-	// Print unit
-	friend std::ostream& operator<<(std::ostream&, Unit*);
+	friend std::ostream& operator<<(std::ostream&, Unit*); // Print the unit's ID
 };
 
 #endif
